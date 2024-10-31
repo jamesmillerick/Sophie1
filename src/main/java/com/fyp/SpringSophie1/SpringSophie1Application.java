5854fb9c369fe2dcf9caa0680d770010f8eb8a4f
jamesmillerick <jamesmillerick8@gmail.com>
2024-10-31 12:30:10 +0000

    Developed code to configure Spring Boot Application. Also my first Commit

---
+++ src/main/java/com/fyp/SpringSophie1/SpringSophie1Application.java
@@ -0,0 +1,15 @@
+package com.fyp.SpringSophie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSophie1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSophie1Application.class, args);
+	}
+
+}
+/* code came from reference 1 of my list of references*/
+
---
+++ src/test/java/com/fyp/SpringSophie1/SpringSophie1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.SpringSophie1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSophie1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
